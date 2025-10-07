3173cee95f1119076f13b307b5d99df4c3abd14e
Trần Quang Tùng <113844686+Tung1312@users.noreply.github.com>
2025-10-07 18:17:27 +0700

    feat(menu): add Score, Story, and Versus mode menus

---
+++ src/main/java/com/birb_birb/blockbounce/ui/menus/ScoreModeMenu.java
@@ -0,0 +1,43 @@
+package com.birb_birb.blockbounce.ui.menus;
+
+import com.almasb.fxgl.app.scene.MenuType;
+import com.birb_birb.blockbounce.constants.GameConstants;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+/**
+ * Score Mode menu.
+ */
+public class ScoreModeMenu extends BaseMenu {
+
+    public ScoreModeMenu() {
+        super(MenuType.GAME_MENU);
+    }
+
+    @Override
+    protected String getBackgroundImagePath() {
+        return GameConstants.SCORE_MODE_BACKGROUND;
+    }
+
+    @Override
+    protected void setupContent() {
+        Button newGameButton = createMenuButton("New Game");
+        newGameButton.setLayoutX(getButtonX());
+        newGameButton.setLayoutY(getStartY());
+        newGameButton.setOnAction(e -> fireNewGame());
+        root.getChildren().add(newGameButton);
+
+        setupKeyHandling();
+    }
+
+    private void setupKeyHandling() {
+        getInput().addAction(new com.almasb.fxgl.input.UserAction("ESC_BACK") {
+            @Override
+            protected void onActionBegin() {
+                getSceneService().popSubScene();
+            }
+        }, KeyCode.ESCAPE);
+    }
+}
---
+++ src/main/java/com/birb_birb/blockbounce/ui/menus/StoryModeMenu.java
@@ -0,0 +1,43 @@
+package com.birb_birb.blockbounce.ui.menus;
+
+import com.almasb.fxgl.app.scene.MenuType;
+import com.birb_birb.blockbounce.constants.GameConstants;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+/**
+ * Story Mode menu.
+ */
+public class StoryModeMenu extends BaseMenu {
+
+    public StoryModeMenu() {
+        super(MenuType.GAME_MENU);
+    }
+
+    @Override
+    protected String getBackgroundImagePath() {
+        return GameConstants.STORY_MODE_BACKGROUND;
+    }
+
+    @Override
+    protected void setupContent() {
+        Button newGameButton = createMenuButton("New Game");
+        newGameButton.setLayoutX(getButtonX());
+        newGameButton.setLayoutY(getStartY());
+        newGameButton.setOnAction(e -> fireNewGame());
+        root.getChildren().add(newGameButton);
+
+        setupKeyHandling();
+    }
+
+    private void setupKeyHandling() {
+        getInput().addAction(new com.almasb.fxgl.input.UserAction("ESC_BACK") {
+            @Override
+            protected void onActionBegin() {
+                getSceneService().popSubScene();
+            }
+        }, KeyCode.ESCAPE);
+    }
+}
