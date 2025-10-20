bd5f08811aad894a65420b51405d5ca2ef2e3f89
Nooirpe <nooirpe@gmail.com>
2025-10-20 12:23:06 +0700

    feat(game): add support for multiple game modes with initialization logic

--- src/main/java/com/birb_birb/blockbounce/core/BlockBounceApp.java
+++ src/main/java/com/birb_birb/blockbounce/core/BlockBounceApp.java
@@ -6,10 +6,13 @@ import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.SceneFactory;
 import com.birb_birb.blockbounce.constants.EntityType;
 import com.birb_birb.blockbounce.constants.GameConstants;
+import com.birb_birb.blockbounce.gamemode.endless.EndlessModeGame;
 import com.birb_birb.blockbounce.entities.PaddleComponent;
+import com.birb_birb.blockbounce.gamemode.story.StoryModeGame;
 import com.birb_birb.blockbounce.ui.menus.MainMenu;
 import com.birb_birb.blockbounce.utils.CursorManager;
 import com.birb_birb.blockbounce.utils.SoundManager;
+import com.birb_birb.blockbounce.gamemode.versus.VersusModeGame;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import org.jetbrains.annotations.NotNull;
@@ -17,6 +20,21 @@ import org.jetbrains.annotations.NotNull;
 import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class BlockBounceApp extends GameApplication {
+
+    // Game mode tracking
+    public enum GameMode {
+        STORY, VERSUS, ENDLESS
+    }
+
+    private static GameMode currentGameMode = GameMode.STORY;
+
+    public static void setGameMode(GameMode mode) {
+        currentGameMode = mode;
+    }
+
+    public static GameMode getCurrentGameMode() {
+        return currentGameMode;
+    }
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -69,7 +87,20 @@ public class BlockBounceApp extends GameApplication {
     @Override
     protected void initGame() {
         CursorManager.apply(getGameScene().getRoot());
-        GameInitializer.initializeGame();
+
+        // Initialize game based on selected mode
+        switch (currentGameMode) {
+            case STORY:
+                StoryModeGame.initialize();
+                break;
+            case VERSUS:
+                VersusModeGame.initialize();
+                break;
+            case ENDLESS:
+                EndlessModeGame.initialize();
+                break;
+        }
+
         getGameScene().setBackgroundColor(Color.BLACK);
     }
 
